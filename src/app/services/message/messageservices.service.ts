4eabb6cd16d99bc5510c20554ffcea1298290542
DiegoBorja09 <diegoborja09@gmail.com>
2022-12-07 17:04:00 -0500

    final

--- src/app/services/message/messageservices.service.ts
+++ src/app/services/message/messageservices.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { Message } from '../../Modelo/Message';
+import { Router } from '@angular/router';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class MessageservicesService {
 
-  constructor() { }
+
+  constructor(private http: HttpClient, private router: Router) { }
+
+
+  url = "http://localhost:4000/message/";
+
+
+  
+  getMessage(){
+    return this.http.get<Message[]>(this.url)
+  }
+
+  getMesasagebyId(id:number){
+    return this.http.get<Message[]>(this.url+"/findone/"+id);
+  }
+
+  createmessage(message:Message){
+    return this.http.post<Message>(this.url+"create",message)
+    
+  }
+
 }
